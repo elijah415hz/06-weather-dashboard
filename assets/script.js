5b9c34d94d4cc4969383d180c1ddab255befce6f
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-09-18 13:43:47 -0700

    Live weather content from openweather API

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var city = "seattle";
+var city = "Seattle";
 console.log("Salmon")
 
 
@@ -21,11 +21,75 @@ $.ajax({
       $.ajax({
         url: weatherURL,
         method: "GET"
-      }).then(function(weather) {
-          console.log(weather);
-          console.log("Temp:", weather.current.temp);
-          console.log("Humidity:", weather.current.humidity);
-          console.log("High Tomorrow:", weather.daily[0].temp.max);
-          console.log("Low Tomorrow:", weather.daily[0].temp.min);
+      }).then(function(response) {
+        loadCurrentWeather(response);
+        loadForecasts(response.daily);
       });
-  })
+    })
+
+    function loadCurrentWeather(weather) {
+      console.log(weather);
+      var currentDate = new Date(weather.current.dt*1000).toLocaleDateString()
+      console.log(currentDate);
+
+      console.log("Temp:", weather.current.temp);
+      console.log("Humidity:", weather.current.humidity);
+      
+
+      var currentWeatherDiv = $("<div class='card-body'>");
+      var cityDate = $("<h2>").text(`${city} (${currentDate})`);
+      var iconCode = weather.current.weather[0].icon
+      var icon = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+iconCode+"@2x.png")
+      var temp = $("<p>").text(`Temperature: ${weather.current.temp} °F`);
+      var humidity = $("<p>").text(`Humidity: ${weather.current.humidity}%`);
+      var windSpeed = $("<p>").text(`Wind Speed: ${weather.current.wind_speed} MPH`);
+      var uv = weather.current.uvi
+      if (uv < 2) {
+        var style = "background-color: green;";
+      } else if (uv >= 2 && uv < 5) {
+      var style = "background-color: yellow;";
+      } else if (uv >= 5 && uv < 7) {
+      var style = "background-color: orange;";
+      } else if (uv >= 7 && uv < 10) {
+      var style = "background-color: red;";
+      } else if (uv >= 10) {
+      var style = "background-color: violet;";
+      }
+      var uvIndex = $("<p>").html(`UV Index: <span style="${style}" id="uv">${uv}<span>`);
+      currentWeatherDiv.append(cityDate, icon, temp, humidity, windSpeed, uvIndex);
+      $("#currentWeather").append(currentWeatherDiv);
+    }
+
+    function loadForecasts(forecasts) {
+      var forecastBox = $("<div class='flex-box'>")
+      for (var i=0; i < 5; i++) {
+        var forecast = forecasts[i]
+        var date = new Date(forecast.dt*1000).toLocaleDateString();
+        var temp = forecast.temp.day
+        var humidity = forecast.humidity
+
+        var forecastCard = $("<div class='card blue'>");
+        var fCardBody = $("<div class='card-body pb-0'>");
+        var fCardTitle = $("<h5 class='card-title'>");
+        var iconCode = forecast.weather[0].icon
+        var icon = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+iconCode+".png")
+     
+        var fCardTemp = $("<p>")
+        var fCardHumidity = $("<p>")
+        fCardTitle.text(date)
+        fCardTemp.text(`Temp: ${temp} °F`)
+        fCardHumidity.text(`Humidity: ${humidity}%`)
+
+        fCardBody.append(fCardTitle, icon, fCardTemp, fCardHumidity);
+        forecastCard.append(fCardBody);
+        forecastBox.append(forecastCard);
+      }
+      $(".forecast").append(forecastBox);
+
+        console.log(new Date(forecast.dt*1000).toLocaleDateString());
+      // console.log("High:", forecast.temp.max);
+      // console.log("Low:", forecast.temp.min);
+
+      //TODO: Dynamically create card, load in date, icon, temp, humidity
+
+    }
